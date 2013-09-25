b45d7e9ee6b59068b8dca01e5093bd7d1ab99826
Toam <thomas.brelet@gmail.com>
2013-09-25 10:01:59 +0200

    Input times in hours

---
+++ lib/redmine_advanced_issues/patches/timelog_helper_patch.rb
@@ -0,0 +1,35 @@
+# Redmine advanced issues - Plugin improve time entry
+# Copyright (C) 2011  Tieu-Philippe Khim
+# Copyright (C) 2012  Yann Autissier
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+require 'redmine_advanced_issues/time_management'
+
+module RedmineAdvancedIssues
+  module Patches
+    module TimelogHelperPatch
+
+      def time(hours)
+        return RedmineAdvancedIssues::TimeManagement.calculate hours, Setting.plugin_redmine_advanced_issues['default_unit']
+      end #time(hours)
+
+      def default_unit_time
+        return RedmineAdvancedIssues::TimeManagement.getDefaultTimeUnit(Setting.plugin_redmine_advanced_issues['default_unit'])
+      end #default_unit_time
+
+    end #TimelogHelperPatch
+  end #Patches
+end #RedmineAdvancedIssues
